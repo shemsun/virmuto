395fa729c8ddffe3d97653fe4456059f24fb2f46
Shekanah Johnson <shokemoke@gmail.com>
2013-08-27 10:32:56 -0400

    created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Virmuto::Application.config.secret_token = '9cd885428a6d891e928fa064f46340fb7b72becd68556adbc2bba4b476a6f8f18396f1a8f49524b9684314f7f663c226b81457b10dfcf262b3131d8a68a63970'
